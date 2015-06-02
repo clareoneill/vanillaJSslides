1f13024b10cb9d13acf3b5b86354c66c95088824
coneill <cmmo330@gmail.com>
2015-06-02 09:31:46 -0500

    store currSlideCount on the class

--- assets/scripts/development/slides.js
+++ assets/scripts/development/slides.js
@@ -1,69 +1,70 @@
 var Slider = (function () {
   
-  /* 	set up variables, init function  */
-	function Slider ( element ) { 
-		this.par = document.querySelector( '.wrap' );
-		this.el = document.querySelector( element );
-		this.slides = this.el.querySelectorAll( '.slide' );
-		this.slideContent = this.el.querySelectorAll( '.content' );
-		this.arrows = document.querySelectorAll( '.arrows svg' );
-		this.w = window;
-		this.baseURL = this.w.location.protocol + '//' + this.w.location.host + this.w.location.pathname;
+  /*  set up variables, init function  */
+  function Slider ( element ) { 
+    this.par = document.querySelector( '.wrap' );
+    this.el = document.querySelector( element );
+    this.slides = this.el.querySelectorAll( '.slide' );
+    this.slideContent = this.el.querySelectorAll( '.content' );
+    this.arrows = document.querySelectorAll( '.arrows svg' );
+    this.w = window;
+    this.baseURL = this.w.location.protocol + '//' + this.w.location.host + this.w.location.pathname;
     this.timer = 0;
-		this.ARROWS = {
-			LEFT: this.par.querySelector( '.left' ),
-			RIGHT: this.par.querySelector( '.right' )
-		};
-		this.CLASSLIST = {
-			ACTIVE: 'active'
-		};
-		this.KEY = {
-		 	LEFT: 37,
-		 	RIGHT: 39
-		};
+    this.currSlideCount = 1;
+    this.ARROWS = {
+      LEFT: this.par.querySelector( '.left' ),
+      RIGHT: this.par.querySelector( '.right' )
+    };
+    this.CLASSLIST = {
+      ACTIVE: 'active'
+    };
+    this.KEY = {
+      LEFT: 37,
+      RIGHT: 39
+    };
 
-		this.init();
+    this.init();
     this.winResize();
-	}
+  }
   
   Slider.prototype.init = function () {
-  	/* 	find window height,
-  			set up empty array for slide heights  */ 
-  	var winHeight = this.w.innerHeight,
-  			slideHeights = [];
+    /*  find window height,
+        set up empty array for slide heights  */ 
+    var winHeight = this.w.innerHeight,
+        slideHeights = [];
 
-  	/* 	find all height of the slide's contents div  */
-  	for( var i = 0, slideHeight, slideOffset = 0; i < this.slideContent.length; i++ ) {
-  		slideHeight = this.slideContent[i].offsetHeight;
-  		/* 	if the slide content height is less than the window height,
-  			 	set slide height and data-height attribute equal to the window height
-  			 	push to slideHeights  */
-  		if( slideHeight <= winHeight ) {
-  			this.slides[i].style.height = winHeight + 'px';
-  			this.slides[i].setAttribute( 'data-height', winHeight );
-  			slideHeights.push( winHeight );
-  		/*	else set data-height attribute to the contents height,
-  				push to slideHeights  */
-  		} else {
-  			this.slides[i].style.height = slideHeight + 'px';
-  			this.slides[i].setAttribute( 'data-height', slideHeight );
-  			slideHeights.push( slideHeight );
-  		}
+    /*  find all height of the slide's contents div  */
+    for( var i = 0, slideHeight, slideOffset = 0; i < this.slideContent.length; i++ ) {
+      slideHeight = this.slideContent[i].offsetHeight;
+      /*  if the slide content height is less than the window height,
+          set slide height and data-height attribute equal to the window height
+          push to slideHeights  */
+      if( slideHeight <= winHeight ) {
+        this.slides[i].style.height = winHeight + 'px';
+        this.slides[i].setAttribute( 'data-height', winHeight );
+        slideHeights.push( winHeight );
+      /*  else set data-height attribute to the contents height,
+          push to slideHeights  */
+      } else {
+        this.slides[i].style.height = slideHeight + 'px';
+        this.slides[i].setAttribute( 'data-height', slideHeight );
+        slideHeights.push( slideHeight );
+      }
 
-  		/*	add up the slide heights */
-  		for( var k = 0, slideDiff = 0; k < slideHeights.length; k++ ) {
-  			slideDiff += slideHeights[k];
-  		}
+      /*  add up the slide heights */
+      for( var k = 0, slideDiff = 0; k < slideHeights.length; k++ ) {
+        slideDiff += slideHeights[k];
+      }
 
-  		/*	find the offset of each slide, set data-offset for each slide */
-  		slideOffset = slideDiff - slideHeights[i];
-  		this.slides[i].setAttribute( 'data-offset', slideOffset );
-  	}
+      /*  find the offset of each slide, set data-offset for each slide */
+      slideOffset = slideDiff - slideHeights[i];
+      this.slides[i].setAttribute( 'data-offset', slideOffset );
+    }
 
-  	/* 	set the wrap height to the first slide's height  */
-  	this.par.style.height = slideHeights[0] + 'px';
+    /*  set the wrap height to the first slide's height  */
+    this.par.style.height = slideHeights[0] + 'px';
 
-  	this.setSlideParam();
+    this.setSlideParam();
   };
 
   Slider.prototype.winResize = function () {
@@ -78,113 +79,114 @@ var Slider = (function () {
   };
 
   Slider.prototype.setSlideParam = function () {
-  	/*	get slide parameter */
-  	var slideParam = getParameterByName( 'slide' ),
-  			currSlide;
+    /*  get slide parameter */
+    var slideParam = getParameterByName( 'slide' ),
+        currSlide;
 
-  	/* 	if slide parameter is empty, set to 1 */
-  	if( slideParam === '' ) {
-  		currSlide = this.slides[ 0 ];
-  		this.w.history.replaceState({}, '', this.baseURL+'?slide=1');
-  		currSlide.classList.add( this.CLASSLIST.ACTIVE );
-  	/*	else set that slide to active */
-  	} else {
-  		currSlide = this.slides[ slideParam - 1 ];
-  		var currSlideHeight = currSlide.getAttribute( 'data-height' ),
-  				currSlideOffset = currSlide.getAttribute( 'data-offset' );
-  		currSlide.classList.add( 'active' );
-  		this.el.style.marginTop = '-' + currSlideOffset + 'px';
-  		this.par.style.height = currSlideHeight + 'px';
-  	}
+    /*  if slide parameter is empty, set to 1 */
+    if( slideParam === '' ) {
+      currSlide = this.slides[ 0 ];
+      this.w.history.replaceState({}, '', this.baseURL+'?slide=1');
+      currSlide.classList.add( this.CLASSLIST.ACTIVE );
+    /*  else set that slide to active */
+    } else {
+      currSlide = this.slides[ slideParam - 1 ];
+      var currSlideHeight = currSlide.getAttribute( 'data-height' ),
+          currSlideOffset = currSlide.getAttribute( 'data-offset' );
+      currSlide.classList.add( 'active' );
+      this.el.style.marginTop = '-' + currSlideOffset + 'px';
+      this.par.style.height = currSlideHeight + 'px';
+    }
 
-  	this.setArrows( currSlide );
+    this.setArrows( currSlide );
   };
 
   Slider.prototype.setArrows = function ( currSlide ) {
-  	/*	get current slide count */
-  	var self = this,
+    /*  get current slide count */
+    var self = this,
         slideTotal = this.slides.length,
-        currSlideCount = parseInt(currSlide.getAttribute('data-count')),
         nextSlideCount;
 
-  	/*	show arrows depending on what the current slide is */
-  	if( currSlideCount === 1 ) {
-  		this.ARROWS.LEFT.classList.remove( this.CLASSLIST.ACTIVE );
-  		this.ARROWS.RIGHT.classList.add( this.CLASSLIST.ACTIVE );
-  	} else if( currSlideCount === this.slides.length ) {
-  		this.ARROWS.LEFT.classList.add( this.CLASSLIST.ACTIVE );
-  		this.ARROWS.RIGHT.classList.remove( this.CLASSLIST.ACTIVE );
-  	} else {
-  		this.ARROWS.LEFT.classList.add( this.CLASSLIST.ACTIVE );
-  		this.ARROWS.RIGHT.classList.add( this.CLASSLIST.ACTIVE );
-  	}
+    this.currSlideCount = parseInt(currSlide.getAttribute('data-count'));
 
-	  /*  on keydown, set set nextSlideCount and call animateSlides() */
+    /*  show arrows depending on what the current slide is */
+    if( this.currSlideCount === 1 ) {
+      this.ARROWS.LEFT.classList.remove( this.CLASSLIST.ACTIVE );
+      this.ARROWS.RIGHT.classList.add( this.CLASSLIST.ACTIVE );
+    } else if( this.currSlideCount === this.slides.length ) {
+      this.ARROWS.LEFT.classList.add( this.CLASSLIST.ACTIVE );
+      this.ARROWS.RIGHT.classList.remove( this.CLASSLIST.ACTIVE );
+    } else {
+      this.ARROWS.LEFT.classList.add( this.CLASSLIST.ACTIVE );
+      this.ARROWS.RIGHT.classList.add( this.CLASSLIST.ACTIVE );
+    }
+
+    /*  on keydown, set set nextSlideCount and call animateSlides() */
     document.onkeydown = function(e) {
       if( e.keyCode == self.KEY.LEFT || e.which == self.KEY.LEFT ) {
-        if( currSlideCount > 1 ) {
-          self.moveLeft( currSlideCount );
+        if( self.currSlideCount > 1 ) {
+          self.moveLeft();
         }
       } else if( e.keyCode == self.KEY.RIGHT || e.which == self.KEY.RIGHT ) {
-        if( currSlideCount < slideTotal ) {
-          self.moveRight( currSlideCount );
+        if( self.currSlideCount < slideTotal ) {
+          self.moveRight();
         }
       }     
     };
 
     /*  arrow left click */
     document.getElementById('arrow-left').onclick = function () {
-      if( currSlideCount > 1 ) {
-        self.moveLeft( currSlideCount );
+      if( self.currSlideCount > 1 ) {
+        self.moveLeft();
       }
     };
 
     /*  arrow right click */
     document.getElementById('arrow-right').onclick = function () {
-      if( currSlideCount < slideTotal ) {
-        self.moveRight( currSlideCount );
+      if( self.currSlideCount < slideTotal ) {
+        self.moveRight();
       }
     };
   };
 
-  Slider.prototype.moveLeft = function ( currSlideCount ) {
+  Slider.prototype.moveLeft = function () {
     /*  get nextSlideCount, animate slides left */
-    nextSlideCount = parseInt(currSlideCount) - 1;
-    this.slides[ currSlideCount - 1 ].classList.remove( this.CLASSLIST.ACTIVE );
+    nextSlideCount = parseInt(this.currSlideCount) - 1;
+    this.slides[ this.currSlideCount - 1 ].classList.remove( this.CLASSLIST.ACTIVE );
     this.animateSlides( nextSlideCount );
   };
 
-  Slider.prototype.moveRight = function ( currSlideCount ) {
+  Slider.prototype.moveRight = function () {
     /*  get nextSlideCount, animate slides right */
-    nextSlideCount = parseInt(currSlideCount) + 1;
-    this.slides[ currSlideCount - 1 ].classList.remove( this.CLASSLIST.ACTIVE );
+    nextSlideCount = parseInt(this.currSlideCount) + 1;
+    this.slides[ this.currSlideCount - 1 ].classList.remove( this.CLASSLIST.ACTIVE );
     this.animateSlides( nextSlideCount );
   };
 
   Slider.prototype.animateSlides = function ( nextSlideCount ) {
-  	/*	get nextSlide, its offset and height */
-		var nextSlide = this.slides[ nextSlideCount - 1 ],
-				nextSlideOffset = nextSlide.getAttribute( 'data-offset' ),
-				nextSlideHeight = nextSlide.getAttribute( 'data-height' );
-		/*	move .slideshow to show the next slide */
-		this.el.style.marginTop = '-' + nextSlideOffset + 'px';
-		/*	set the wrap height to the next slide's height */
-		this.par.style.height = nextSlideHeight + 'px';
-		/*	update the URL slide parameter */
-		this.w.history.replaceState({}, '', this.baseURL+'?slide=' + nextSlideCount);
+    /*  get nextSlide, its offset and height */
+    var nextSlide = this.slides[ nextSlideCount - 1 ],
+        nextSlideOffset = nextSlide.getAttribute( 'data-offset' ),
+        nextSlideHeight = nextSlide.getAttribute( 'data-height' );
+    /*  move .slideshow to show the next slide */
+    this.el.style.marginTop = '-' + nextSlideOffset + 'px';
+    /*  set the wrap height to the next slide's height */
+    this.par.style.height = nextSlideHeight + 'px';
+    /*  update the URL slide parameter */
+    this.w.history.replaceState({}, '', this.baseURL+'?slide=' + nextSlideCount);
 
-		this.setArrows( nextSlide );
+    this.setArrows( nextSlide );
   };
   
   return Slider;
 }());
 
 document.addEventListener( 'DOMContentLoaded', function() {
-	var slider = new Slider( '.slideshow' );
+  var slider = new Slider( '.slideshow' );
 });
 
 
-/*	get URL parameter */
+/*  get URL parameter */
 function getParameterByName(name) {
   name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
   var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
