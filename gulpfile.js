1432a136f1dd4bcdba9978dc088bafcb2d00f2d3
Clare O'Neill <coneill@pixelfarm.com>
2016-03-03 14:10:31 -0600

    update gulpfile and dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,26 @@
 // include gulp
 var gulp = require('gulp'); 
 
-// include Our plugins
-var jshint = require('gulp-jshint');
-var minify = require('gulp-minify-css');
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
+// include gulp plugins
+var minify = require( 'gulp-minify-css' ),
+    sass = require( 'gulp-sass' ),
+    rename = require( 'gulp-rename' ),
+    uglify = require( 'gulp-uglify' ),
+    concat = require( 'gulp-concat' ),
+    jshint = require( 'gulp-jshint' ),
+    autoprefixer = require('gulp-autoprefixer');
 
-// compile our sass plugins to css
-gulp.task('sass-plugins', function() {
-  return gulp.src('assets/styles/development/sass/_plugins/*.scss')
-    .pipe(sass())
-    .pipe(rename('plugins.css'))
-    .pipe(gulp.dest('assets/styles/developmentcss'))
-    .pipe(minify())
-    .pipe(rename('plugins.min.css'))
-    .pipe(gulp.dest('assets/styles/production'));
-});
+/* default task */
+gulp.task('default', ['sass', 'lint', 'scripts']);
 
 // compile our site sass
 gulp.task('sass', function() {
-  return gulp.src('assets/styles/development/sass/*.scss')
+  return gulp.src('assets/styles/development/scss/*.scss')
     .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
     .pipe(gulp.dest('assets/styles/development/css'))
     .pipe(minify())
     .pipe(rename(function (path) {
@@ -32,21 +29,11 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('assets/styles/production'));
 });
 
-// lint task
+// lint js
 gulp.task('lint', function() {
   return gulp.src('assets/scripts/development/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
-});
-
-// concatenate & minify js plugins
-gulp.task('js-plugins', function() {
-  return gulp.src('assets/scripts/development/_plugins/*.js')
-    .pipe(concat('_plugins.js'))
-    .pipe(gulp.dest('assets/scripts/development'))
-    .pipe(rename('plugins.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('assets/scripts/production'));
 });
 
 // concatenate & minify js
@@ -59,14 +46,13 @@ gulp.task('scripts', function() {
     .pipe(gulp.dest('assets/scripts/production'));
 });
 
-// watch files For changes
-gulp.task('watch', function() {
-  gulp.watch('assets/styles/development/sass/plugins/*.scss', ['sass-plugins']);
-  gulp.watch('assets/styles/development/sass/*.scss', ['sass']);
-  gulp.watch('assets/styles/development/sass/_plugins/*.js', ['js-plugins']);
-  gulp.watch('assets/scripts/development/_plugins/*.js', ['lint', 'js-plugins']);
-  gulp.watch('assets/scripts/development/*.js', ['lint', 'scripts']);
-});
+/* watch for changes */
+gulp.task('watch', ['default'], function() {
+  var watchFiles = [
+    'assets/styles/development/scss/_partials/*.scss',
+    'assets/styles/development/scss/*.scss',
+    'assets/scripts/development/*.js',
+  ];
 
-// default task
-gulp.task('default', ['sass-plugins', 'sass', 'lint', 'js-plugins', 'scripts', 'watch']);
+  gulp.watch(watchFiles, ['default']);
+});
